2f4701bdf60f371442d28f0efcd952e000388153
shinrai2 <fuck@shinrai.cc>
2017-06-15 01:22:01 +0800

    加入RSA代码.未接入.

--- src/server/Backstage.java
+++ src/server/Backstage.java
@@ -26,17 +26,17 @@ public class Backstage implements ThreadCallBack {
 	}
 	
 	/**
-	 * 给定请求中的参数 'account' 'password'，查询数据库和登录表，判断是否允许登录。
+	 * 给定请求中的参数 'nickname' 'password'，查询数据库和登录表，判断是否允许登录。
 	 * 情况分为：
 	 * 1. 账号密码与数据库记录相同，登录表中不存在该账号，将账号置入登录表并返回 true。
 	 * 2. 账号密码与数据库记录相同，登录表中存在该账号，广播询问登录情况，视广播情况返回 true 或 false。（有点难度）
 	 * 3. 账号对应密码不相同，返回 false。
 	 * 4. 不存在此账号信息，添加到数据库和登录表并返回 true。（即新建账号操作）
-	 * @param account 请求登录的账号名。
+	 * @param nickname 请求登录的昵称。
 	 * @param password 请求登录的密码（md5）
 	 * @return 是否允许登录：true - 允许 / false - 不允许
 	 */
-	private boolean isLogin(String account, String password) {
+	private boolean isLogin(String nickname, String password) {
 		
 		return false;
 	}
