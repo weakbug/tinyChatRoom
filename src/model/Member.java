aa888b65cabf7899968bec41caaaa94dc8fe60bf
shinrai2 <fuck@shinrai.cc>
2017-06-15 17:53:50 +0800

    信息记录模块改动；登录请求状态细化。

--- src/model/Member.java
+++ src/model/Member.java
@@ -8,23 +8,63 @@ public class Member {
 
 	private String nickname;
 	private String password;
-	private String ip;
-	private String publicKey;
+	private String ipAddress;
 	private RsaUtil rsaUtil;
+	private boolean isLogin;
 	
-	/** 实例化时生成rsa实例 */
-	private Member() {
+	/**
+	 * 主构造器
+	 * @param nickname
+	 * @param password
+	 * @param ipAddress
+	 */
+	public Member(String nickname, String password, String ipAddress) {
+		/** 实例化时生成rsa实例 */
 		rsaUtil = new RsaUtil();
-	}
-	public Member(String nickname, String password, String ip, String publicKey) {
-		this();
+		isLogin = false;
 		this.nickname 	= nickname;
 		this.password 	= password;
-		this.ip 				= ip;
-		this.publicKey 	= publicKey;
+		this.ipAddress 	= ipAddress;
+	}
+	/**
+	 * 辅用构造器
+	 * 用于验证登录，用完丢弃
+	 * @param nickname
+	 */
+	public Member(String nickname) {
+		this.nickname 	= nickname;
 	}
 	
+	@Override
+	public boolean equals(Object anObject) {
+		if(this == anObject) {
+			return true;
+		}
+		if(anObject instanceof Member) {
+			Member anotherMember = (Member)anObject;
+			if(this.getNickname() == anotherMember.getNickname()) {
+				return true;
+			}
+			return false;
+		}
+		return false;
+	}
+	
+	/** five getter functions below */
+	public String getNickname() {
+		return nickname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getIpAddress() {
+		return ipAddress;
+	}
 	public RsaUtil getRsaUtil() {
 		return rsaUtil;
 	}
+	public boolean isLogin() {
+		return isLogin;
+	}
+	
 }
--- src/server/Backstage.java
+++ src/server/Backstage.java
@@ -14,6 +14,9 @@ public class Backstage implements ThreadCallBack {
 	
 	private List<Member> loginList;
 	private UdpUtil udpUtil;
+	private enum loginStatus {
+		ALL_RIGHT, MAYBE_REPEAT, WRONG_PASSWORD, NEW_ACCOUNT;
+	}
 	
 	public Backstage() {
 		initialize();
@@ -26,19 +29,34 @@ public class Backstage implements ThreadCallBack {
 	}
 	
 	/**
-	 * 给定请求中的参数 'nickname' 'password'，查询数据库和登录表，判断是否允许登录。
+	 * 给定请求中的参数 'nickname' 'password'，查询数据表，判断是否允许登录。
 	 * 情况分为：
-	 * 1. 账号密码与数据库记录相同，登录表中不存在该账号，将账号置入登录表并返回 true。
-	 * 2. 账号密码与数据库记录相同，登录表中存在该账号，广播询问登录情况，视广播情况返回 true 或 false。（有点难度）
+	 * 1. 账号密码与数据表记录相同，'isLogin' 标记未登录，更改标记并返回 true。
+	 * 2. 账号密码与数据表记录相同，'isLogin' 标记已登录，广播询问登录情况，视广播情况返回 true 或 false。（有点难度）
 	 * 3. 账号对应密码不相同，返回 false。
-	 * 4. 不存在此账号信息，添加到数据库和登录表并返回 true。（即新建账号操作）
+	 * 4. 不存在此账号信息，添加到数据表、置 'isLogin' 标记并返回 true。（即新建账号操作）
 	 * @param nickname 请求登录的昵称。
 	 * @param password 请求登录的密码（md5）
 	 * @return 是否允许登录：true - 允许 / false - 不允许
 	 */
-	private boolean isLogin(String nickname, String password) {
-		
-		return false;
+	private loginStatus isLogin(String nickname, String password) {
+		Member __new = new Member(nickname);
+		int index_of_member = loginList.indexOf(__new);
+		if(index_of_member == -1) { //不存在账号（即新建账号操作）
+			return loginStatus.NEW_ACCOUNT;
+		}
+		else {
+			Member origin_member = loginList.get(index_of_member);
+			if( !(origin_member.getPassword().equals(password)) ) { //密码错误
+				return loginStatus.WRONG_PASSWORD;
+			}
+			else if(origin_member.isLogin()) { //标记正在登录
+				return loginStatus.MAYBE_REPEAT;
+			}
+			else { //正常登录
+				return loginStatus.ALL_RIGHT;
+			}
+		}
 	}
 	
 	/**
