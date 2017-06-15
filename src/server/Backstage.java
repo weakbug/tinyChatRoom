c604e0dc013bfe92e797e4f9df98bf75bfd0f26a
shinrai2 <fuck@shinrai.cc>
2017-06-16 01:03:55 +0800

    广播情况分组.

--- src/server/Backstage.java
+++ src/server/Backstage.java
@@ -31,10 +31,10 @@ public class Backstage implements ThreadCallBack {
 	/**
 	 * 给定请求中的参数 'nickname' 'password'，查询数据表，判断是否允许登录。
 	 * 情况分为：
-	 * 1. 账号密码与数据表记录相同，'isLogin' 标记未登录，更改标记并返回 true。
-	 * 2. 账号密码与数据表记录相同，'isLogin' 标记已登录，广播询问登录情况，视广播情况返回 true 或 false。（有点难度）
-	 * 3. 账号对应密码不相同，返回 false。
-	 * 4. 不存在此账号信息，添加到数据表、置 'isLogin' 标记并返回 true。（即新建账号操作）
+	 * 1. 账号密码与数据表记录相同，'isLogin' 标记未登录，更改标记并返回 ALL_RIGHT。
+	 * 2. 账号密码与数据表记录相同，'isLogin' 标记已登录，返回 MAYBE_REPEAT 并广播询问登录情况，视广播情况回应广播。（有点难度）
+	 * 3. 账号对应密码不相同，返回 WRONG_PASSWORD。
+	 * 4. 不存在此账号信息，添加到数据表、置 'isLogin' 标记并返回 NEW_ACCOUNT。（即新建账号操作）
 	 * @param nickname 请求登录的昵称。
 	 * @param password 请求登录的密码（md5）
 	 * @return 是否允许登录：true - 允许 / false - 不允许
